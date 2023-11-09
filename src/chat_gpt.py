13d28abfd0da192143aac23b77fec576056f8c7a
jnuo <onurovalii@gmail.com>
2023-11-09 16:51:11 +0300

    able to contact chatgpt api successfully now added a mock way to not consume my credits

--- src/chat_gpt.py
+++ src/chat_gpt.py
@@ -1,6 +1,9 @@
 from dotenv import load_dotenv
 import openai
 import os
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+import config
 
 # load the .env file
 load_dotenv()
@@ -10,10 +13,16 @@ openai.api_key = os.getenv('OPENAI_API_KEY')
 
 def ask_chat_gpt(prompt, temperature=0.7, max_tokens=150):
     """Ask a question to ChatGPT and return the response."""
-    response = openai.completions.create(
-        model="gpt-3.5-turbo",
-        prompt=prompt,
-        temperature=temperature,
-        max_tokens=max_tokens
-    )
-    return response.choices[0].text.strip()
+    
+    if(config.USE_OPENAI_API):
+        response = openai.chat.completions.create(
+            model="gpt-4",
+            messages=[
+                {"role": "system", "content": "You are a helpful assistant."},
+                {"role": "user", "content": prompt}
+            ]
+        )
+        return response.choices[0].message.content
+    else:
+        return "Hola, ¿cómo estás?"
+
